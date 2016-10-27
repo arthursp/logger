981cf1b6f71516c2646a9f0da472d87904324a54
Arthur <arthur.spitznagel@outlook.fr>
2016-10-27 12:30:22 +0200

    Base version of logger

---
+++ index.js
@@ -0,0 +1,20 @@
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http , {origins : '*:*'});
+
+io.on('connection', (socket) => {
+  console.log('user connected');
+  
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  
+  socket.on('add-message', (message) => {
+  	console.log(message);
+    io.emit('message', {type:'new-message', text: message});    
+  });
+});
+
+http.listen(5000, () => {
+  console.log('started on port 5000');
+});
